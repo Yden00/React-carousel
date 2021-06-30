469ac5523a74bff9e14dfd3f8ecfb8d0d7d55f21
Vlad Teterev <vladtett2000@gmail.com>
2021-06-30 13:45:48 +0300

    feat: finished dots component

--- src/components/Carousel.component.jsx
+++ src/components/Carousel.component.jsx
@@ -9,28 +9,38 @@ const Carousel = () => {
   const currentIndex = useRef(Math.floor(slides.length / 2))
   function renderSlide(slide, index) {
     return (
-      <li key={index}>
+      <li className="slide" key={index}>
         <h3>{slide.title}</h3>
         <img src={slide.image} />
       </li>
     )
   }
 
+  function changeCurrentIndex(newIndex) {
+    currentIndex.current = newIndex
+    if (currentIndex.current === 0) {
+      setCurrentSlides([slides[slides.length - 1], slides[currentIndex.current], slides[currentIndex.current + 1]]);
+    }
+    else if (currentIndex.current + 1 === slides.length) {
+      setCurrentSlides([slides[currentIndex.current - 1], slides[currentIndex.current], slides[0]]);
+    }
+    else if (currentIndex.current < slides.length - 1) {
+      setCurrentSlides([slides[currentIndex.current - 1], slides[currentIndex.current], slides[currentIndex.current + 1]]);
+    }
+  }
+
   function displaySlidesRight() {
     if (currentIndex.current === slides.length - 1) {
       currentIndex.current = 0;
       setCurrentSlides([slides[slides.length - 1], slides[currentIndex.current], slides[currentIndex.current + 1]]);
-      console.log('Первый if', currentIndex.current);
     }
     else if (currentIndex.current + 2 === slides.length) {
       currentIndex.current++;
       setCurrentSlides([slides[currentIndex.current - 1], slides[currentIndex.current], slides[0]]);
-      console.log('Второй if', currentIndex.current);
     }
     else if (currentIndex.current < slides.length - 2) {
       currentIndex.current++;
       setCurrentSlides([slides[currentIndex.current - 1], slides[currentIndex.current], slides[currentIndex.current + 1]]);
-      console.log('Третий if', currentIndex.current);
     }
   }
 
@@ -38,30 +48,26 @@ const Carousel = () => {
     if (currentIndex.current === 0) {
       currentIndex.current = slides.length - 1;
       setCurrentSlides([slides[slides.length - 2], slides[currentIndex.current], slides[0]])
-      console.log('Первый if', currentIndex.current)
     }
     else if (currentIndex.current === 1) {
       currentIndex.current--
       setCurrentSlides([slides[slides.length - 1], slides[currentIndex.current], slides[currentIndex.current + 1]])
-      console.log('Второй if', currentIndex.current)
     }
     else if (currentIndex.current <= slides.length - 1) {
       currentIndex.current--
       setCurrentSlides([slides[currentIndex.current - 1], slides[currentIndex.current], slides[currentIndex.current + 1]])
-      console.log('Третий if', currentIndex.current)
     }
   }
-
   return (
     <div className='carousel'>
-      <ul>
+      <ul className="slider">
         <button className="left" onClick={displaySlidesLeft}></button>
         {
           currentSlides.map((el, index) => renderSlide(el, index))
         }
         <button className="right" onClick={displaySlidesRight}></button>
       </ul>
-      <DotsList />
+      <DotsList currentIndex={currentIndex.current} changeCurrentIndex={changeCurrentIndex} />
     </div>
   )
 }
--- src/components/Dots.component.jsx
+++ src/components/Dots.component.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CarouselData } from './carouselData'
 import './Dots.css'
 
 const DotsList = (props) => {
+    function goTo(index) {
+    props.changeCurrentIndex(index)
+  }
   return (
     <ul className="dots-list">
       {
-        CarouselData.map((el,index) => {
-          <li></li>
-        })
+        CarouselData.map((el, index) => {
+          if (index === props.currentIndex) {
+            return <li className="current-dot" onClick={() => { goTo(index) }} key={index}></li>
+          }
+          else {
+            return <li className="dot" onClick={() => { goTo(index) }} key={index}></li>
+          }
+        }
+        )
       }
     </ul>
   )
